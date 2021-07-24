f7e4b47ed839a1b81b1d160ab17f2303b5a8554c
Jacob Guiro <jguiro09@gmail.com>
2021-07-23 21:49:08 -0500

    Inmplemented manager organization

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const inquirer = require('inquirer');
-const table = require ('console.table');
 const e = require('./lib/Employee');
-const db = require('./lib/db');
+const questions = require('./lib/questions');
+const tableData = require('./lib/tableData');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -11,43 +11,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 function askQuestions() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            name: 'answer',
-            choices: ['view all employees', "add employee", "update employee role", "view all roles", "add role", "view all departments", "add department", "quit"]
-        },
-        {
-            type: 'input',
-            message: "What is your new employee's first name?",
-            name: "eFName",
-            when: (list) => list.answer == "add employee"
-        },
-        {
-            type: 'input',
-            message: "What is your new employee's last name?",
-            name: "eLName",
-            when: (list) => list.answer == "add employee"
-        },
-        {
-            type: 'input',
-            message: "What is your new employee's roleID?",
-            name: "eRole",
-            when: (list) => list.answer == "add employee"
-        }
-    ])
+    inquirer.prompt(questions.initialQuestions)
     .then((response) =>
     {
-        console.log(response);
-        console.log("done!");
-
         switch(response.answer){
             case 'view all employees':
                 e.viewEmployee();
                 break;
             case "quit":
-                e.getRole();
+                tableData.getRoles();
                 break;
             case "add employee":
                 e.addEmployee(response.eFName, response.eLName, response.eRole);
