e77b109daf4e5b6a2ebf14a5031227fe86b93179
Jacob Guiro <jguiro09@gmail.com>
2021-07-28 10:45:37 -0500

    questions added and completed

--- server.js
+++ server.js
@@ -11,13 +11,32 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+// ? -------------------------------------------------------------------- INITIAL QUESTIONS ---------------------------------------------------------------------------------------
 function askQuestions() {
     inquirer.prompt(q.initialQuestions)
     .then((response) =>
     {
         switch(response.answer){
             case "Employees":
-                inquirer.prompt(q.employeeOptions)
+                askEmployeeQuestions();
+                break;
+
+            case "Departments":
+                askDepartmentQuestions();
+                break;
+
+            case "Roles":
+                askRoleQuestions();
+                break;
+        }
+    })
+}
+
+
+// ? -------------------------------------------------------------------- EMPLOYEE QUESTIONS ---------------------------------------------------------------------------------------
+function askEmployeeQuestions() {
+    inquirer.prompt(q.employeeOptions)
                     .then((response) => {
                         switch (response.eOption)
                         {
@@ -26,10 +45,13 @@ function askQuestions() {
                                     .then((response) => {
                                         switch(response.eView){
                                             case 'View All Employees':
+                                                e.viewEmployee(askQuestions);
                                                 break;
                                             case 'View Employees By Manager':
+                                                e.viewEmployeeByManager(askQuestions);
                                                 break;
                                             case 'View Employees By Department':
+                                                e.viewEmployeeByDepartments(askQuestions);
                                                 break;
                                         }
                                     })
@@ -39,47 +61,57 @@ function askQuestions() {
                                 .then((response) => {
                                     switch(response.eUpdate){
                                         case "Add Employee":
-                                            inquirer.prompt(questions.addEmployee)
-                                                .then((response) => {
-                                                    e.addEmployee(response.eFName, response.eLName, response.eRole, response.eManager);})
+                                            inquirer.prompt(q.addEmployee)
+                                                .then((response) => {e.addEmployee(response.eFName, response.eLName, response.eRole, response.eManager,askQuestions);})
                                             break;
                                         case "Update Existing Employee Traits":
-                                            inquirer.prompt(questions.updateEmployee)
+                                            inquirer.prompt(q.updateEmployee)
                                             .then((response) => {
                                                 if(response.eUpdate == "role")
-                                                {e.updateRole(response.eName, response.eRole)}
+                                                    {e.updateRole(response.eName, response.eRole,askQuestions)}
                                                 else if (response.eUpdate == "manager")
-                                                {e.updateManager(response.eName, response.eManager)}
+                                                    {e.updateManager(response.eName, response.eManager,askQuestions)}
                                             })
                                             break;
                                         case "Delete Employee":
+                                            inquirer.prompt(q.deleteEmployee)
+                                            .then((response) => {e.deleteEmployee(response.name,askQuestions)})
                                             break;
                                     }
                                 })
                                 break;
                         }
                     })
-                break;
+}
 
-            case "Departments":
-                inquirer.prompt(q.departmentQuestions)
+// ? -------------------------------------------------------------------- DEPARTMENT QUESTIONS ---------------------------------------------------------------------------------------
+function askDepartmentQuestions(){
+    inquirer.prompt(q.departmentQuestions)
                 .then((response) => {
                     switch(response.dOption)
                     {
                         case "View All Departments":
+                            d.viewDepartments(askQuestions);
                             break;
                         case "Add Departments":
+                            inquirer.prompt(q.addDepartment)
+                            .then((response) => {
+                                d.addDepartment(response.name, askQuestions);
+                            })
                             break;
                         case "Delete Departments":
+                            q.deleteDepartment()
                             break;
                         case "Check Utilizied Budget of Departments":
+                            d.showDepartmentsBudget(askQuestions);
                             break;
                     }
                 })
-                break;
+}
 
-            case "Roles":
-                inquirer.prompt(q.roleQuestions)
+// ? -------------------------------------------------------------------- ROLE QUESTIONS ---------------------------------------------------------------------------------------
+function askRoleQuestions(){
+    inquirer.prompt(q.roleQuestions)
                 .then((response) => {
                     switch(response.option)
                     {
@@ -89,19 +121,19 @@ function askQuestions() {
                                 switch(response.viewOption)
                                 {
                                     case "View All Roles":
+                                        r.viewRoles(askQuestions);
                                         break;
                                     case "View Roles By Department":
+                                        inquirer.prompt(q.roleViewDepartment).then((response) => {r.ViewRolesByDepartment(response.department, askQuestions)})
                                         break;
                                 }
                             })
                             break;
-
                         case 'Add a New Role':
+                            r.addRole(askQuestions);
                             break;
                     }
                 })
-        }
-    })
 }
 
 askQuestions();
